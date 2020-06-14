83173a6749d9d30eeb1d95b5b5d2691d95fab92b
WinIT23 <chauhanvinit23@gmail.com>
2020-06-14 21:33:43 +0530

    Censor all p tags.....

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-console.log("Hello content!!!");
+console.log("Censoring texts!!!");
+
+var paragraphs = document.getElementsByTagName('p');
+for (elt of paragraphs) {
+	elt.style['background-color'] = "black";
+	elt.style['color'] = "black";
+}
