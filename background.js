6e4a621e5659c701815db80d1ff90cc9386119f2
WinIT23 <chauhanvinit23@gmail.com>
2020-06-15 11:54:09 +0530

    Basic password chacking.

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
 console.log("Hey There background working");
+
+var isFirst = false;
+
+
+function pass(str) {
+	isFirst = true;
+	var prom = prompt("Enter your password : " + str, "");
+
+	if (prom === "1234") {
+		console.log("Sucessful login !!!");
+	} else {
+		pass("(Wrong Password)");
+	}
+}
+
+chrome.runtime.onStartup.addListener(function() {
+	pass("");
+});
